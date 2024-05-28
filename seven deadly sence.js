d4eb6361f57d48e8871b96532b3db497d23adb99
myfirstperfectproject <soumyagajendra499@gmail.com>
2024-05-28 21:22:36 +0530

    new file

---
+++ seven deadly sence.js
@@ -0,0 +1,7 @@
+var cursor=document.querySelector('#light');
+document.addEventListener ('mousemove',function(e){
+    var x = e.clientX;
+    var y = e.clientY;
+    cursor.style.left= x + 'px'
+    cursor.style.top = y +'px';
+})
