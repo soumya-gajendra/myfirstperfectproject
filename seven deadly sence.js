c935960de64a48e0f6068c07088f11d165766367
myfirstperfectproject <soumyagajendra499@gmail.com>
2024-06-11 23:17:41 +0530

    ascl

--- seven deadly sence.js
+++ seven deadly sence.js
@@ -1,7 +1,25 @@
 var cursor=document.querySelector('#light');
+var text=document.querySelector('.heading');
 document.addEventListener ('mousemove',function(e){
     var x = e.clientX;
     var y = e.clientY;
     cursor.style.left= x + 'px'
-    cursor.style.top = y +'px';
+    cursor.style.top = y +'px'
+})
+const target = document.querySelectorAll('info');
+const targetmove = (x,y) => 'perspecttive(500px) scale(1.1) rotateX(${x}deg) rotateY(${y}deg)'
+target.forEach(info =>{
+     const heigth = info.offsetHeigth
+     const width = info.offsetWeigth
+
+     info.addEventListener('mousemove',(e) => {
+        const x = e.clientX
+        const y = e.clientY
+        
+
+        const l = 50*((x-width / 2) / width)
+        const k = -50*((y-height / 2) /height)
+        info.style.transfrom = targetmove(l,k)
+     })
+  info.addEventListener('mouseout',() => p.style.transfrom = targetmove(0,0))
 })
